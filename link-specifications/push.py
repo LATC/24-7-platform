4e06224415700961f65421742164ae086cd37c88
Christophe Gueret <christophe.gueret@gmail.com>
2012-01-18 17:02:44 +0100

    Synchronized specs

--- link-specifications/push.py
+++ link-specifications/push.py
@@ -11,10 +11,11 @@ import cStringIO
 import json
 import urllib
 import urllib2
-from datetime import datetime
 import time
 import re
 import md5
+from datetime import datetime
+from xml.dom.minidom import parse, parseString
 
 TEST = "http://127.0.0.1:8080/console/api"
 PRODUCTION = "http://latc-console.few.vu.nl/api"
@@ -39,107 +40,114 @@ def synchronize_directory(dir):
         print messages.get(res) % task
 
 def push_new(dir, task, spec):
-    '''
-    Push a new file to the server
-    '''
-    meta_file = '%s/%s/README.txt' % (dir, task)
-    spec_file = '%s/%s/spec.xml' % (dir, task)
+	'''
+	Push a new file to the server
+	'''
+	meta_file = '%s/%s/README.txt' % (dir, task)
+	spec_file = '%s/%s/spec.xml' % (dir, task)
     
-    # Read meta information and generate title
-    meta = {}
-    if os.path.exists(meta_file):
-        tmp = map(lambda x:re.sub(r'[\r\n]+', '', x), open(meta_file, 'r').readlines())
-        meta = dict((tmp[i * 2], tmp[i * 2 + 1]) for i in range(len(tmp) / 2))
-    tmp = task.split('-')
-    meta['Title:'] = "%s -> %s (%s)" % (tmp[0], tmp[1], "".join(tmp[2:]))
-
-    # Upload the file
-    curl = pycurl.Curl()
-    response = cStringIO.StringIO()
-    values = [
-              ('api_key', 'aa4967eb8b7a5ccab7dbb57aa2368c7f'),
-              ('specification', spec),
-              ('title', meta['Title:'])
-    ]
-    if 'Description:' in meta.keys():
-        values.append(('description', meta['Description:']))
-    if 'Creator:' in meta.keys():
-        values.append(('author', meta['Creator:']))
-    curl.setopt(curl.URL, SERVER + "/tasks")
-    curl.setopt(curl.POSTFIELDS, urllib.urlencode(values))
-    curl.setopt(curl.WRITEFUNCTION, response.write)
-    curl.perform()
-
-    # Get the ID
-    if curl.getinfo(pycurl.HTTP_CODE) != 201:
-        print SERVER + "/tasks"
-        print curl.getinfo(pycurl.HTTP_CODE)
-        print values
-        print response.getvalue()
-    res = json.loads(response.getvalue())
-    curl.close()
-    
-    return res['id']
+	if os.path.exists(spec_file):
+		# Read meta information and generate title
+		meta = {}
+		if os.path.exists(meta_file):
+			tmp = map(lambda x:re.sub(r'[\r\n]+', '', x), open(meta_file, 'r').readlines())
+			meta = dict((tmp[i * 2], tmp[i * 2 + 1]) for i in range(len(tmp) / 2))
+			tmp = task.split('-')
+			meta['Title:'] = "%s -> %s (%s)" % (tmp[0], tmp[1], "".join(tmp[2:]))
+		
+		# Upload the file
+		curl = pycurl.Curl()
+		response = cStringIO.StringIO()
+		values = [
+			('api_key', 'aa4967eb8b7a5ccab7dbb57aa2368c7f'),
+			('specification', spec),
+			('title', meta['Title:'])
+		]
+		if 'Description:' in meta.keys():
+			values.append(('description', meta['Description:']))
+		if 'Creator:' in meta.keys():
+			values.append(('author', meta['Creator:']))
+		curl.setopt(curl.URL, SERVER + "/tasks")
+		curl.setopt(curl.POSTFIELDS, urllib.urlencode(values))
+		curl.setopt(curl.WRITEFUNCTION, response.write)
+		curl.perform()
+		
+		# Get the ID
+		if curl.getinfo(pycurl.HTTP_CODE) != 201:
+			print SERVER + "/tasks"
+			print curl.getinfo(pycurl.HTTP_CODE)
+			print values
+			print response.getvalue()
+		res = json.loads(response.getvalue())
+		curl.close()
+		
+		return res['id']
+	else:
+		return 0
 
 def push_update(id, spec):
-    '''
-    Push an updated file to the server
-    '''
-    curl = pycurl.Curl()
-    data = [
-         ('api_key', 'aa4967eb8b7a5ccab7dbb57aa2368c7f'),
-         ('configuration', spec)
-    ]
-    body = urllib.urlencode(data)
-    curl.setopt(curl.URL, SERVER + '/task/' + id + '/configuration')
-    curl.setopt(curl.HTTPHEADER, [ 'Content-Type:application/x-www-form-urlencoded; charset=utf-8', 'Expect: ']);
-    curl.setopt(curl.UPLOAD, 1)
-    request_buffer = cStringIO.StringIO(body)
-    curl.setopt(pycurl.READFUNCTION, request_buffer.read)
-    curl.setopt(curl.INFILESIZE, len(body))
-    response = cStringIO.StringIO()
-    curl.setopt(curl.WRITEFUNCTION, response.write)
-    curl.perform()
-    curl.close()
+	'''
+	Push an updated file to the server
+	'''
+	curl = pycurl.Curl()
+	data = [
+	     ('api_key', 'aa4967eb8b7a5ccab7dbb57aa2368c7f'),
+	     ('configuration', spec)
+	]
+	body = urllib.urlencode(data)
+	curl.setopt(curl.URL, SERVER + '/task/' + id + '/configuration')
+	curl.setopt(curl.HTTPHEADER, [ 'Content-Type:application/x-www-form-urlencoded; charset=utf-8', 'Expect: ']);
+	curl.setopt(curl.UPLOAD, 1)
+	request_buffer = cStringIO.StringIO(body)
+	curl.setopt(pycurl.READFUNCTION, request_buffer.read)
+	curl.setopt(curl.INFILESIZE, len(body))
+	response = cStringIO.StringIO()
+	curl.setopt(curl.WRITEFUNCTION, response.write)
+	curl.perform()
+	curl.close()
     
 def synchronize_task(dir, task):
-    '''
-    Push a specific task and return a status code
-    '''
-    spec_file = '%s/%s/spec.xml' % (dir, task)
-    spec_local = "".join(open(spec_file).readlines())
-    id_file = '%s/%s/id.txt' % (dir, task)
-    
-    # Retrieve the ID from a previous upload
-    id = None
-    if os.path.isfile(id_file):
-        id = open(id_file).readlines()[0]
-        
-    # If there is no ID, upload the file
-    if id == None:
-        id = push_new(dir, task, spec_local)
-        open(id_file, 'w').write(id)
-        return UPLOADED
-    else:
-        # Dowload the current specification from the server
-        spec_server = urllib2.urlopen(SERVER + '/task/' + id + '/configuration').read()
-        
-        # Compare the two versions
-        disk = md5.new(spec_local).hexdigest()
-        server = md5.new(spec_server).hexdigest()
-        
-        # Push an update if needed
-        if disk == server:
-            return UP_TO_DATE            
-        else:
-            #push_update(id, spec_local)
-            return UPDATED
-        
-    # We should not reach that line
-    return ERROR
+	'''
+	Push a specific task and return a status code
+	'''
+	spec_file = '%s/%s/spec.xml' % (dir, task)
+	if os.path.exists(spec_file):
+		spec_local = "".join(open(spec_file).readlines())
+	    	
+		id_file = '%s/%s/id.txt' % (dir, task)
+		
+		# Retrieve the ID from a previous upload
+		id = None
+		if os.path.isfile(id_file):
+			id = open(id_file).readlines()[0]
+		
+		# If there is no ID, upload the file
+		if id == None:
+			id = push_new(dir, task, spec_local)
+			open(id_file, 'w').write(id)
+			return UPLOADED
+		else:
+			# Dowload the current specification from the server
+			spec_server = urllib2.urlopen(SERVER + '/task/' + id + '/configuration').read()
+		
+		# Compare the two versions
+		try:
+			disk = md5.new(parseString(spec_local).toxml()).hexdigest()
+			server = md5.new(parseString(spec_server).toxml()).hexdigest()
+			
+			# Push an update if needed
+			if disk == server:
+				return UP_TO_DATE            
+			else:
+				push_update(id, spec_local)
+				return UPDATED
+		except:
+			pass
+		
+	# We should not reach that line
+	return ERROR
 
 
 if __name__ == '__main__':
-    synchronize_directory('.')
-
-
+	synchronize_directory('.')
+	
