a92d9a1ceb7b24060f645e6de7fca3a96ab24d50
cstadler <cstadler@informatik.uni-leipzig.de>
2012-02-09 16:41:51 +0100

    Improved output of QA bash script

--- latc-platform/linkqa/src/main/bash/to-rdf.sh
+++ latc-platform/linkqa/src/main/bash/to-rdf.sh
@@ -4,16 +4,22 @@ id="$1"
 params="$2"
 polarity="$3"
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 source "$params"
 
-source "config-$polarity.ini"
+source "$DIR/config-$polarity.ini"
 
 echo "<$1> <$p_precision> \"$precision\"^^<http://www.w3.org/2001/XMLSchema#float> ."
 echo "<$1> <$p_recall> \"$recall\"^^<http://www.w3.org/2001/XMLSchema#float> ."
 echo "<$1> <$p_estimatedPrecisionLowerBound> \"$estimatedPrecisionLowerBound\"^^<http://www.w3.org/2001/XMLSchema#float> ."
 echo "<$1> <$p_estimatedPrecisionUpperBound> \"$estimatedPrecisionLowerBound\"^^<http://www.w3.org/2001/XMLSchema#float> ."
-echo "<$1> <$p_endDate> \"$rawLinksetSize\"^^<http://www.w3.org/2001/XMLSchema#long> ."
+echo "<$1> <$p_rawRefsetSize> \"$rawRefsetSize\"^^<http://www.w3.org/2001/XMLSchema#long> ."
+echo "<$1> <$p_intersectionSize> \"$intersectionSize\"^^<http://www.w3.org/2001/XMLSchema#long> ."
 echo "<$1> <$p_rawLinksetSize> \"$rawLinksetSize\"^^<http://www.w3.org/2001/XMLSchema#long> ."
 echo "<$1> <$p_linksetErrorCount> \"$linksetErrorCount\"^^<http://www.w3.org/2001/XMLSchema#long> ."
+echo "<$1> <$p_refsetDuplicateSize> \"$refsetDuplicateSize\"^^<http://www.w3.org/2001/XMLSchema#long> ."
+echo "<$1> <$p_linksetDuplicateSize> \"$linksetDuplicateSize\"^^<http://www.w3.org/2001/XMLSchema#long> ."
+echo "<$1> <$p_refsetErrorCount> \"$refsetErrorCount\"^^<http://www.w3.org/2001/XMLSchema#long> ."
 
 
