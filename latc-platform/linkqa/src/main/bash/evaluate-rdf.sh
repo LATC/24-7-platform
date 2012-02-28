b1d42fbe7a9244d243cf5b310fae4975314a8b6a
cstadler <cstadler@informatik.uni-leipzig.de>
2012-02-28 17:38:57 +0100

    Java wrapper for the evaluate-rdf bash script

--- latc-platform/linkqa/src/main/bash/evaluate-rdf.sh
+++ latc-platform/linkqa/src/main/bash/evaluate-rdf.sh
@@ -11,10 +11,23 @@ polarity="$4"
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ ! -f "$ls" -o ! -f "$rs" -o -z "$id" ]; then
+error=0
+if [ ! -f "$ls" ]; then
+	echo "$ls not found"
+	error=1
+fi
+
+if [ ! -f "$rs" ]; then
+        echo "$rs not found"
+        error=1
+fi
+ 
+
+
+if [ "$error" -eq 1 -o -z "$id" ]; then
         echo "Usage: Linkset.nt Referenceset.nt http://your.linkset.uri [polarity]"
         echo "Note: polarity can be either positive or negative. If polarity is not given, and refset filename contains pos or neg, polarity is automatically set accordingly."
-        exit -1
+        exit 1
 fi
 
 
@@ -29,7 +42,7 @@ fi
 
 if [ -z "$polarity" ]; then
 	echo "Polarity (positive/negative) either not given or it could not be determined from the refset name"
-	exit -1
+	exit 1
 fi
 
 
