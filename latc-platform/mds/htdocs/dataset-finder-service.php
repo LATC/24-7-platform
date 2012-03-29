18911b852d39b7f9ea9670b2d4c60de7fdb0a861
Keith Alexander <github@keithalexander.co.uk>
2012-03-29 16:45:46 +0100

    added CORS support to dataset finder

--- latc-platform/mds/htdocs/dataset-finder-service.php
+++ latc-platform/mds/htdocs/dataset-finder-service.php
@@ -3,6 +3,7 @@ require '../scripts/inc.php';
 
 if(isset($_GET['text'])) {
   header("Content-type: application/json");
+  header("Access-Control-Allow-Origin: *");
   $text = $_GET['text'];
   $output = findDatasetsMatchingText($text);  
   echo json_encode($output);
